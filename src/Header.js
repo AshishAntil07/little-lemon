f377dce64b03d52865faef7594d94b69f132a414
Ashish Antil <ashishantil.antil07@gmail.com>
2023-02-11 12:36:11 +0530

    Add style to Hero and Header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,18 @@
-function Header(){
+export default function Header(){
   return(
     <nav>
-      {
-        ['']
-      }
+      <img src='../pictures/logo.png' />
+      <ul>
+        {
+          ['Home','About','Order','Testimonials'].map(str => {
+            return(
+              <li>
+                <a href={`#${str.toLowerCase()}`}>{str}</a>
+              </li>
+            )
+          })
+        }
+      </ul>
     </nav>
   )
 }
-
-export default Header
--- src/Hero.js
+++ src/Hero.js
@@ -1,10 +1,15 @@
 export default function Hero(){
   return (
     <section className='hero'>
-      <header>Little Lemon</header>
-      <p>
-        Little Lemon is a restaurant that delivers delicious meals at an affordable price to its customers on time. We offer lemonades, tortillas, and vegetables like potatoes, onions, etc. We also offer a variety of fruits. So if you also want to have some meal, order your food beside. <br /> Have a delicious day!
-      </p>
+      <section className='about'>
+        <header>Little Lemon</header>
+        <p>
+          Little Lemon is a restaurant that delivers delicious meals at an affordable price to its customers on time. We offer lemonades, tortillas, and vegetables like potatoes, onions, etc. We also offer a variety of fruits. So if you also want to have some meal, order your food <span className='rel'></span>. <br /> Have a delicious day!
+        </p>
+      </section>
+      <section className='form'>
+
+      </section>
     </section>
   )
 }
